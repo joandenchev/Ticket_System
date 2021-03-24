d5af0a07a9900d4deadc1edea00508a567f15711
Yonchi03 <JoanDenchev@gmail.com>
2021-03-24 16:52:24 +0200

    0.5.1 Date & Time added

--- Ticket_System/Plays.java
+++ Ticket_System/Plays.java
@@ -1,22 +1,27 @@
 package Ticket_System;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 
 //TO-DO: ADD DATABASE SUPPORT!
 
 public class Plays {
-    private       int    playNumber;
-    private       String playName;
-    private final short  playPrice;
+    private       int     playNumber;
+    private       String  playName;
+    private final short   playPrice;
+    private final LocalDateTime dateTime;
 
     protected LinkedList<Tickets> ticketList = new LinkedList<>();
     private   static int   playNumberCounter = 0;
 
-    public Plays(String playName, short price) {
+    public Plays(String playName, short price, String dateTime) {
         this.playNumber = ++playNumberCounter;
         this.playName   = playName;
         this.playPrice  = price;
+        this.dateTime   = LocalDateTime.parse(dateTime, formatter);
     }
 
     public String getPlayName() { return playName; }
@@ -26,7 +31,9 @@ public class Plays {
     public void setPlayNumber(short playNumber) { this.playNumber = playNumber; }
 
     public short getPlayPrice() { return playPrice; }
+    public LocalDateTime getDateTime() { return dateTime; }
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH-mm : dd/MM/yyyy");
 
     public float getProfit(@NotNull Plays play){
         float profit = 0;
