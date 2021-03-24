25044a6b2b63e148747b534842872b9500a012d0
Yonchi03 <JoanDenchev@gmail.com>
2021-03-24 15:26:36 +0200

    Cleanup

--- Ticket_System/Plays.java
+++ Ticket_System/Plays.java
@@ -11,21 +11,12 @@ public class Plays {
     private final short  playPrice;
 
     protected LinkedList<Tickets> ticketList = new LinkedList<>();
-
-    private static int playNumberCounter = 0;
+    private   static int   playNumberCounter = 0;
 
     public Plays(String playName, short price) {
-        this.playName = playName;
         this.playNumber = ++playNumberCounter;
-        this.playPrice = price;
-    }
-
-    public float getProfit(@NotNull Plays play){
-        float profit = 0;
-        for (Tickets p : play.ticketList) {
-            profit = profit + (p.getTicketPrice()*p.getSeatsReserved());
-        }
-        return profit;
+        this.playName   = playName;
+        this.playPrice  = price;
     }
 
     public String getPlayName() { return playName; }
@@ -35,4 +26,13 @@ public class Plays {
     public void setPlayNumber(short playNumber) { this.playNumber = playNumber; }
 
     public short getPlayPrice() { return playPrice; }
+
+
+    public float getProfit(@NotNull Plays play){
+        float profit = 0;
+        for (Tickets p : play.ticketList) {
+            profit = profit + (p.getTicketPrice()*p.getSeatsReserved());
+        }
+        return profit;
+    }
 }
